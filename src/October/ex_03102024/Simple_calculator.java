9aa375cde7b157cce2305bf67a0ce0347d3cc2af
Akshatha S M <akshathasm@Akshathas-Laptop.local>
2024-10-04 06:55:57 +0530

    Initial commit

---
+++ src/October/ex_03102024/Simple_calculator.java
@@ -0,0 +1,21 @@
+package October.ex_03102024;
+
+public class Simple_calculator {
+    public static void main(String[] args)
+    {
+        System.out.println("Simple Calculator program");
+        int a = 30;
+        int b = 20;
+
+        System.out.println("Addition of two numbers is" + "=" + (a + b));
+        System.out.println("");
+        System.out.println("Subtraction of two numbers is" + "=" + (a - b));
+        System.out.println("");
+        System.out.println("Multiplication of two numbers is" + "=" + (a * b));
+        System.out.println("");
+        System.out.println("Division of two numbers is" + "=" + (a / b));
+        System.out.println("");
+
+    }
+
+}
